449ea068b3c9ab9183c537205478750ded5e8e5d
bingfeng <260895778@qq.com>
2022-09-23 15:08:32 +0800

    85

--- src/pages/allLesson/chapter12-着色器打造烟雾水云效果/lesson84-85-使用官方提供的水模型.vue
+++ src/pages/allLesson/chapter12-着色器打造烟雾水云效果/lesson84-85-使用官方提供的水模型.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue';
 import BaseCanvas from '../../../components/BaseCanvas.vue';
-import { ACESFilmicToneMapping, AxesHelper, BoxGeometry, CineonToneMapping, Clock, Color, DoubleSide, EquirectangularReflectionMapping, LinearToneMapping, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneBufferGeometry, RawShaderMaterial, ReinhardToneMapping, Scene, ShaderMaterial, sRGBEncoding, TextureLoader, Vector2, WebGLRenderer } from 'three'
+import { ACESFilmicToneMapping, AxesHelper, BoxGeometry, CineonToneMapping, Clock, Color, DirectionalLight, DoubleSide, EquirectangularReflectionMapping, LinearToneMapping, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneBufferGeometry, RawShaderMaterial, ReinhardToneMapping, Scene, ShaderMaterial, sRGBEncoding, TextureLoader, Vector2, WebGLRenderer } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
@@ -29,7 +29,7 @@ onMounted(() => {
     const camera = new PerspectiveCamera(75, width / height, .1, 1000)
 
     // 设置相机位置
-    camera.position.set(0, 0, 2)
+    camera.position.set(0, 5, 5)
 
     scene.add(camera)
 
@@ -40,31 +40,60 @@ onMounted(() => {
     scene.add(axesHelper)
 
 
-    // 水的材质
-    const water = new Water(
-        new PlaneBufferGeometry(1, 1, 1024, 1024),
-        {
-            color: "#ffffff",
-            scale: 1,
-            flowDirection: new Vector2(1, 1),
-            textureHeight: 1024,
-            textureWidth: 1024
-        }
-    )
+    // // 水的材质
+    // const water = new Water(
+    //     new PlaneBufferGeometry(1, 1, 1024, 1024),
+    //     {
+    //         color: "#ffffff",
+    //         scale: 1,
+    //         flowDirection: new Vector2(1, 1),
+    //         textureHeight: 1024,
+    //         textureWidth: 1024
+    //     }
+    // )
 
 
-    water.rotation.x = -Math.PI / 2
+    // water.rotation.x = -Math.PI / 2
 
 
-    scene.add(water)
+    // scene.add(water)
 
     // 加载场景背景
     const rgbeLoader = new RGBELoader()
-    rgbeLoader.loadAsync("/050.hdr").then((texture)=>{
+    rgbeLoader.loadAsync("/050.hdr").then((texture) => {
         texture.mapping = EquirectangularReflectionMapping
         scene.background = texture
         scene.environment = texture
     })
+
+    // 加载浴缸
+    const gltfLoader = new GLTFLoader()
+    gltfLoader.load("/model/yugang.glb", (gltf) => {
+        console.log(gltf);
+        const yugang = gltf.scene.children[0]
+        yugang.material.side = DoubleSide
+
+        const waterGeometry = gltf.scene.children[1].geometry
+
+
+        const water = new Water(
+            waterGeometry,
+            {
+                color: "#ffffff",
+                scale: 1,
+                flowDirection: new Vector2(1, 1),
+                textureHeight: 1024,
+                textureWidth: 1024
+            }
+        )
+
+        scene.add(water)
+        scene.add(yugang)
+    })
+
+    // 灯光
+    const directionalLight = new DirectionalLight(0xffffff, 0.5)
+    scene.add(directionalLight)
 
     // 初始化渲染器
     const renderer = new WebGLRenderer({
